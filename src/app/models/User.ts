5fa37c5f081e1902df9f40471378212ff2442fc8
cuongnh <hungcuongqn86@gmail.com>
2020-07-19 16:42:12 +0700

    role

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -2,30 +2,31 @@ import {Partner} from './Partner';
 import {Transaction} from './Transaction';
 
 export interface Role {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
+  position: string;
 }
 
 export interface User {
-    id: number;
-    partner_id: number;
-    partner: Partner;
-    transaction: Transaction[];
-    name: string;
-    email: string;
-    password: string;
-    c_password: string;
-    phone_number: string;
-    role_id: number;
-    roles: Role[];
-    image: string;
-    type: number;
-    debt: number;
-    cost_percent: number;
-    rate: number;
-    deposit: string;
-    weight_price: number;
-    is_deleted: number;
-    created_at: string;
-    updated_at: string;
+  id: number;
+  partner_id: number;
+  partner: Partner;
+  transaction: Transaction[];
+  name: string;
+  email: string;
+  password: string;
+  c_password: string;
+  phone_number: string;
+  role_id: number;
+  roles: Role[];
+  image: string;
+  type: number;
+  debt: number;
+  cost_percent: number;
+  rate: number;
+  deposit: string;
+  weight_price: number;
+  is_deleted: number;
+  created_at: string;
+  updated_at: string;
 }
