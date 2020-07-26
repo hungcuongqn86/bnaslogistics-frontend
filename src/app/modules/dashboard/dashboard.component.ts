c541289c9ba5df4b0cb54caaf2a94318a959a23e
cuongnh <hungcuongqn86@gmail.com>
2020-07-27 01:26:02 +0700

    dashboard

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,20 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {DashboardService} from '../../services/dashboard.service';
+import {AuthService} from "../../auth.service";
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 
 export class DashboardComponent implements OnInit {
 
-    constructor(public dashboardService: DashboardService, private router: Router) {
-
+  constructor(public dashboardService: DashboardService, private router: Router, public authService: AuthService) {
+    if (authService.hasRole('custumer')) {
+      this.router.navigate(['/order/myorder/0/od']);
     }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-    }
+  }
 }
