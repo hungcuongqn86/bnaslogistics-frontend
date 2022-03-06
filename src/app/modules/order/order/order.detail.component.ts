44d6c33144f3100d915960b9b839df6f52fc22c6
cuongnh <hungcuongqn86@gmail.com>
2022-03-07 03:40:05 +0800

    order update

--- src/app/modules/order/order/order.detail.component.ts
+++ src/app/modules/order/order/order.detail.component.ts
@@ -3,36 +3,36 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {OrderService} from '../../../services/order/order.service';
 
 @Component({
-    selector: 'app-order-detail',
-    templateUrl: './order.detail.component.html',
-    styleUrls: ['./order.detail.component.css']
+  selector: 'app-order-detail',
+  templateUrl: './order.detail.component.html',
+  styleUrls: ['./order.detail.component.css']
 })
 
 export class OrderDetailComponent implements OnInit {
-    constructor(private router: Router, private route: ActivatedRoute
-        , public orderService: OrderService) {
-        this.orderService.order_renew();
-        this.route.params.subscribe(params => {
-            if (params['id']) {
-                this.orderService.orderRe.id = params['id'];
-                this.getOrder();
-            }
+  constructor(private router: Router, private route: ActivatedRoute
+    , public orderService: OrderService) {
+    this.orderService.order_renew();
+    this.route.params.subscribe(params => {
+      if (params['id']) {
+        this.orderService.orderRe.id = params['id'];
+        this.getOrder();
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  private getOrder() {
+    if (this.orderService.orderRe.id !== null) {
+      this.orderService.getOrder(this.orderService.orderRe.id)
+        .subscribe(order => {
+          this.orderService.orderRe = order.data;
         });
     }
+  }
 
-    ngOnInit() {
-    }
-
-    private getOrder() {
-        if (this.orderService.orderRe.id !== null) {
-            this.orderService.getOrder(this.orderService.orderRe.id)
-                .subscribe(order => {
-                    this.orderService.orderRe = order.data;
-                });
-        }
-    }
-
-    public backlist() {
-        this.router.navigate(['/order/list']);
-    }
+  public backlist() {
+    this.router.navigate(['/order/list']);
+  }
 }
