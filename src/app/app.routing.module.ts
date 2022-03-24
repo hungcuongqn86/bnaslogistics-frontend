332ba6059e257b91e1390d2cd6cbd8a8e1719cdd
cuongnh <hungcuongqn86@gmail.com>
2022-03-25 00:48:17 +0800

    AppGuard

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,11 +1,10 @@
 import {ModuleWithProviders} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {AppGuard} from './app.guard.service';
 import {Error404Component} from './messages/error404.component';
 import {LoginComponent} from './auth/login.component';
 import {RegisterComponent} from './auth/register.component';
 import {DefaultLayoutComponent} from './layout';
-import {SettingModule} from './modules/setting/setting.module';
 import {ResetPasswordComponent} from './auth/reset.password.component';
 import {ResetComponent} from './auth/reset.component';
 import {ActiveComponent} from './auth/active.component';
@@ -23,122 +22,106 @@ const appRoutes: Routes = [
         loadChildren: './modules/dashboard/dashboard.module#DashboardModule',
         data: {
           title: 'Bảng điều khiển'
-        },
-        canActivate: [AppGuard]
+        }
       },
       {
         path: 'dashboard',
         loadChildren: './modules/dashboard/dashboard.module#DashboardModule',
         data: {
           title: 'Bảng điều khiển'
-        },
-        canActivate: [AppGuard]
+        }
       },
       {
         path: 'notification',
         loadChildren: './modules/notification/notification.module#NotificationModule',
         data: {
           title: 'Thông báo'
-        },
-        canActivate: [AppGuard]
+        }
       }
       , {
         path: 'mpartner',
         loadChildren: './modules/mpartner/mpartner.module#MpartnerModule',
         data: {
           title: 'Đối tác'
-        },
-        canActivate: [AppGuard]
+        }
       }
       , {
         path: 'muser',
         loadChildren: './modules/muser/muser.module#MuserModule',
         data: {
           title: 'Người dùng'
-        },
-        canActivate: [AppGuard]
+        }
       }, {
         path: 'account',
         loadChildren: './modules/account/account.module#AccountModule',
         data: {
           title: 'Tài khoản'
-        },
-        canActivate: [AppGuard]
+        }
       }
       , {
         path: 'mcustumer',
         loadChildren: './modules/mcustumer/mcustumer.module#McustumerModule',
         data: {
           title: 'Khách hàng'
-        },
-        canActivate: [AppGuard]
+        }
       }
       , {
         path: 'cart',
         loadChildren: './modules/cart/cart.module#CartModule',
         data: {
           title: 'Giỏ hàng'
-        },
-        canActivate: [AppGuard]
+        }
       }, {
         path: 'shipping',
         loadChildren: './modules/shipping/shipping.module#ShippingModule',
         data: {
           title: 'Yêu cầu ký gửi'
-        },
-        canActivate: [AppGuard]
+        }
       }
       , {
         path: 'order',
         loadChildren: './modules/order/order.module#OrderModule',
         data: {
           title: 'Đơn hàng'
-        },
-        canActivate: [AppGuard]
+        }
       }, {
         path: 'complain',
         loadChildren: './modules/complain/complain.module#ComplainModule',
         data: {
           title: 'Khiếu nại'
-        },
-        canActivate: [AppGuard]
+        }
       }, {
         path: 'package',
         loadChildren: './modules/package/package.module#PackageModule',
         data: {
           title: 'Kiện hàng'
-        },
-        canActivate: [AppGuard]
+        }
       }, {
         path: 'mypackage',
         loadChildren: './modules/mypackage/mypackage.module#MypackageModule',
         data: {
           title: 'Kiện hàng'
-        },
-        canActivate: [AppGuard]
+        }
       }, {
         path: 'warehouse',
         loadChildren: './modules/warehouse/warehouse.module#WarehouseModule',
         data: {
           title: 'Kho Việt Nam'
-        },
-        canActivate: [AppGuard]
+        }
       }
       , {
         path: 'wallet',
         loadChildren: './modules/wallet/wallet.module#WalletModule',
         data: {
           title: 'Ví điện tử'
-        },
-        canActivate: [AppGuard]
+        }
       }
       , {
         path: 'setting',
         loadChildren: './modules/setting/setting.module#SettingModule',
         data: {
           title: 'Cấu hình hệ thống'
-        },
-        canActivate: [AppGuard]
+        }
       }
     ],
     canActivate: [AppGuard]
