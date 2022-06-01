0eb730369487fe272da8d414ed212ea01623246d
cuongnh <hungcuongqn86@gmail.com>
2022-06-02 02:48:07 +0700

    tao bao

--- src/app/modules/warehousetq/bag/detail.component.ts
+++ src/app/modules/warehousetq/bag/detail.component.ts
@@ -19,7 +19,6 @@ import {Bag} from '../../../models/model';
 export class DetailComponent implements OnInit, OnDestroy {
   bag: IBag;
   package_code: string;
-  packages: IPackage[] = [];
   errorMessage: string[] = [];
   sub: Subscription;
   modalRef: BsModalRef;
@@ -77,31 +76,9 @@ export class DetailComponent implements OnInit, OnDestroy {
     }
   }
 
-  public createStoreBill() {
-    if (this.packages.length) {
-      this.errorMessage = [];
-      this.warehouseService.showLoading(true);
-      const pkidlist = [];
-      for (let i = 0; i < this.packages.length; i++) {
-        pkidlist.push(this.packages[i].id);
-      }
-
-      this.sub = this.warehouseService.bagCreate(pkidlist, this.note_tq, this.dvvc)
-        .subscribe(res => {
-          this.warehouseService.showLoading(false);
-          this.sub.unsubscribe();
-          if (res.status) {
-            this.router.navigate([`/warehouse-tq/bag`]);
-          } else {
-            this.errorMessage = res.data;
-          }
-        });
-    }
-  }
-
   public addPackage(item: IPackage) {
     let found = false;
-    for (const element of this.packages) {
+    for (const element of this.bag.package) {
       if (element.id === item.id) {
         found = true;
         break;
@@ -109,14 +86,14 @@ export class DetailComponent implements OnInit, OnDestroy {
     }
 
     if (!found) {
-      this.packages.push(item);
+      this.bag.package.push(item);
     }
   }
 
   public removePackage(item: IPackage) {
-    const index = this.packages.indexOf(item);
+    const index = this.bag.package.indexOf(item);
     if (index !== -1) {
-      this.packages.splice(index, 1);
+      this.bag.package.splice(index, 1);
     }
   }
 
