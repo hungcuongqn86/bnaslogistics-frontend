22011445efd92f219b4a385a4a11e7364b7ecbe4
cuongnh <hungcuongqn86@gmail.com>
2022-10-10 16:20:46 +0700

    css

--- src/app/auth/register.component.ts
+++ src/app/auth/register.component.ts
@@ -22,7 +22,7 @@ export class RegisterComponent implements OnInit, OnDestroy {
   public register: Register;
   public rError;
   public sSuccess;
-  public alert;
+  public alert: string[];
   public sub: Subscription;
 
   constructor(private router: Router, private authService: AuthService) {
@@ -50,17 +50,22 @@ export class RegisterComponent implements OnInit, OnDestroy {
         if (res.status) {
           this.sSuccess = true;
           this.rError = false;
-          this.alert = `Đăng ký thành công! Chúng tôi gửi cho bạn 1 link kích hoạt đến địa chỉ email: ${this.register.email}` +
+          const tb = `Đăng ký thành công! Chúng tôi gửi cho bạn 1 link kích hoạt đến địa chỉ email: ${this.register.email}` +
             `hãy kiểm tra hòm thư và kích hoạt tài khoản!`;
+          this.alert = [tb];
         } else {
           this.rError = true;
           this.sSuccess = false;
-          this.alert = res.data[0];
+          this.alert = res.data;
         }
+        this.sub.unsubscribe();
+        this.authService.showLoading(false);
       }, (error) => {
         this.rError = true;
         this.sSuccess = false;
-        this.alert = error.message;
+        this.alert = [error.message];
+        this.sub.unsubscribe();
+        this.authService.showLoading(false);
       });
   }
 
