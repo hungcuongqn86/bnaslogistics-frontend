632526ec0cd40377d9867be6f78f69862dc8e1fd
hungcuongqn86 <hungcuongqn86@gmail.com>
2023-07-13 14:45:27 +0800

    hủy

--- src/app/pipes/status.ts
+++ src/app/pipes/status.ts
@@ -1,18 +1,18 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {OrderStatus} from "../models/interface";
+import {OrderStatus} from '../models/interface';
 
 @Pipe({
-    name: 'tempStatus'
+  name: 'tempStatus'
 })
 export class TempStatusPipe implements PipeTransform {
-    transform(code: number, status: OrderStatus[]): string {
-        if (status && status.length) {
-            for (let i = 0; i < status.length; i++) {
-                if (status[i].id === code) {
-                    return status[i].name;
-                }
-            }
+  transform(code: number, status: OrderStatus[]): string {
+    if (status && status.length) {
+      for (let i = 0; i < status.length; i++) {
+        if (status[i].id === code) {
+          return status[i].name;
         }
-        return '';
+      }
     }
+    return '';
+  }
 }
